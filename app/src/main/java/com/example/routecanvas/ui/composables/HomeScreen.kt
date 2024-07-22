5db697d94659eb29559a24cea3cdbbc0b58b4312
shivamparihar12 <parihar.shivam0789@gmail.com>
2024-07-22 14:46:43 +0530

    top app bar setuped with menu

--- app/src/main/java/com/example/routecanvas/ui/composables/HomeScreen.kt
+++ app/src/main/java/com/example/routecanvas/ui/composables/HomeScreen.kt
@@ -1,23 +1,26 @@
 package com.example.routecanvas.ui.composables
 
-import android.widget.Button
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Card
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -34,12 +37,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import com.example.routecanvas.R
 import com.example.routecanvas.ui.theme.RouteCanvasTheme
 import kotlin.math.roundToInt
 
-// n x 2 grid list to show track with top bar
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navigateTo: (route: Screens.TrackScreen) -> Unit) {
@@ -53,6 +54,7 @@ fun HomeScreen(navigateTo: (route: Screens.TrackScreen) -> Unit) {
     val topAppBarOffsetHeightPx = remember { mutableStateOf(0f) }
     var previousIndex by remember { mutableStateOf(0) }
     var previousScrollOffset by remember { mutableStateOf(0) }
+    var showMenu by remember { mutableStateOf(false) }
 
     Surface(modifier = Modifier.fillMaxSize()) {
         Box {
@@ -85,27 +87,48 @@ fun HomeScreen(navigateTo: (route: Screens.TrackScreen) -> Unit) {
             }
 
             TopAppBar(
-                title = { Text(text = R.string.app_name.toString()) },
+                title = { Text(text = "RouteCanvas") },
                 modifier = Modifier
-                    .height(topAppBarHeight)
-                    .offset { IntOffset(x = 0, y = topAppBarOffsetHeightPx.value.roundToInt()) }
+                    .offset { IntOffset(x = 0, y = topAppBarOffsetHeightPx.value.roundToInt()) },
+                actions = {
+                    IconButton(onClick = { showMenu = true }) {
+                        Icon(imageVector = Icons.Filled.Menu, contentDescription = "Menu")
+                    }
+
+                    DropdownMenu(expanded = showMenu, onDismissRequest = { showMenu = false }) {
+                        DropdownMenuItem(
+                            text = { Text(text = "Run") },
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Filled.PlayArrow,
+                                    contentDescription = "Start running"
+                                )
+                            },
+                            onClick = { /*TODO start running > plan out how to proceed from here */ }
+                        )
+
+                        DropdownMenuItem(
+                            text = { Text(text = "About") },
+                            onClick = { /*TODO move to about about compose*/ },
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Filled.Info,
+                                    contentDescription = "About Page"
+                                )
+                            }
+                        )
+                    }
+                }
             )
         }
 
     }
     var toolbarOffsetHeightPx = remember { mutableStateOf(0f) }
     LaunchedEffect(gridState) {
-        snapshotFlow { gridState.firstVisibleItemIndex to gridState.firstVisibleItemScrollOffset}
-            .collect { (index,scrollOffset) ->
-//                val scrolEffect = gridState.firstVisibleItemScrollOffset
-//
-//                val toolbarOffset = when {
-//                    index == 0 -> scrolEffect.toFloat()
-//                    index > 0 -> -topAppBarHeightPx
-//                    previosIndex.value > index -> -topAppBarHeightPx
-//                    else -> 0f
-//                }
-                val isScrollingUp = index < previousIndex || (index == previousIndex && scrollOffset < previousScrollOffset)
+        snapshotFlow { gridState.firstVisibleItemIndex to gridState.firstVisibleItemScrollOffset }
+            .collect { (index, scrollOffset) ->
+                val isScrollingUp =
+                    index < previousIndex || (index == previousIndex && scrollOffset < previousScrollOffset)
 
                 val toolbarOffset = when {
                     index == 0 -> -scrollOffset.toFloat()
@@ -113,8 +136,8 @@ fun HomeScreen(navigateTo: (route: Screens.TrackScreen) -> Unit) {
                     else -> -topAppBarHeightPx
                 }
                 toolbarOffsetHeightPx.value = toolbarOffset.coerceIn(-topAppBarHeightPx, 0f)
-                previousIndex =index
-                previousScrollOffset=scrollOffset
+                previousIndex = index
+                previousScrollOffset = scrollOffset
             }
     }
     topAppBarOffsetHeightPx.value = toolbarOffsetHeightPx.value
